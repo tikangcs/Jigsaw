76cdb015de9870a2963d715af1eb213008228085
Tim Kang <tikang.cs@gmail.com>
2021-04-29 09:50:24 -0400

    #15 - installed PM2

--- db/schema.sql
+++ db/schema.sql
@@ -21,26 +21,6 @@ CREATE TABLE related (
   currentprodid integer,
   relatedprodid integer);
 
--- LOAD DATA INFILE '/var/lib/mysql/data/product-10000.csv'
--- INTO TABLE products
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE '/var/lib/mysql/styles-10000.csv'
--- INTO TABLE styles
--- FIELDS TERMINATED BY ','
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
--- LOAD DATA LOCAL INFILE '/var/lib/mysql/related-10000.csv'
--- INTO TABLE related
--- FIELDS TERMINATED BY ','
--- LINES TERMINATED BY '\n'
--- IGNORE 1 ROWS;
-
 -- LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/product.csv'
 -- INTO TABLE products
 -- FIELDS TERMINATED BY ','
