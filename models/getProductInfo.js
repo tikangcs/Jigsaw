f8f2cf98a65002b7e4a092d261c46a8830cb8be6
Tim Kang <tikang.cs@gmail.com>
2021-04-29 15:30:31 -0400

    #20 - Minor change to syntax in model

--- models/getProductInfo.js
+++ models/getProductInfo.js
@@ -5,14 +5,18 @@ require("dotenv").config();
 module.exports = (id, callback) => {
   client.get(id, (err, payload) => {
     if (err) {
-      console.error(`error getting product ${id} from cache: ${err}`)
+      console.error(`error getting product ${id} from cache: ${err}`);
     }
     if (payload) {
-      res.status(200).send(payload)
+      res.status(200).send(payload);
     } else {
-      db.query(`SELECT * FROM products WHERE PRODID = ${id}`, (err, payload) => {
-      client.setex(id, 240, payload);
-      callback(err, payload);
+      db.query(
+        `SELECT * FROM products WHERE PRODID = ${id}`,
+        (err, payload) => {
+          client.setex(id, 240, payload);
+          callback(err, payload);
+        }
+      );
     }
   });
 };
