cbfef5f1de5db74a2207280fc3d274c7a43d29b6
Tim Kang <tikang.cs@gmail.com>
2021-05-18 12:57:31 -0400

    #32 - Prepping for rev proxy test without redis

--- models/getProductInfo.js
+++ models/getProductInfo.js
@@ -2,21 +2,29 @@ const db = require("../db/index.js");
 const client = require("../server/redis.js");
 require("dotenv").config();
 
+//Without Redis
 module.exports = (id, callback) => {
-  client.get(id, (err, payload) => {
-    if (err) {
-      console.error(`error getting product ${id} from cache: ${err}`);
-    }
-    if (payload) {
-      callback(err, payload);
-    } else {
-      db.query(
-        `SELECT * FROM products WHERE PRODID = ${id}`,
-        (err, payload) => {
-          client.setex(id, 240, JSON.stringify(payload));
-          callback(err, payload);
-        }
-      );
-    }
+  db.query(`SELECT * FROM products WHERE PRODID = ${id}`, (err, payload) => {
+    callback(err, payload);
   });
 };
+
+//With Redis
+// module.exports = (id, callback) => {
+//   client.get(id, (err, payload) => {
+//     if (err) {
+//       console.error(`error getting product ${id} from cache: ${err}`);
+//     }
+//     if (payload) {
+//       callback(err, payload);
+//     } else {
+//       db.query(
+//         `SELECT * FROM products WHERE PRODID = ${id}`,
+//         (err, payload) => {
+//           client.setex(id, 240, JSON.stringify(payload));
+//           callback(err, payload);
+//         }
+//       );
+//     }
+//   });
+// };
